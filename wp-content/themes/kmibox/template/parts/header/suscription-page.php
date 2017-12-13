68d96298e46b10d6e3871f9441fa0988c97b7e2e
kharlos <32462475+yookharlos@users.noreply.github.com>
2017-12-13 14:45:53 -0400

    arreglo de boton de regreso

--- wp-content/themes/kmibox/template/parts/header/suscription-page.php
+++ wp-content/themes/kmibox/template/parts/header/suscription-page.php
@@ -1,16 +1,30 @@
-<header id="header-purchase" class="row">
-	
-				
-	<div id="header-purchase-title" class="col-sm-12 text-center">
-			<a class="btn btn-sm btn-kmibox-white pull-left hidden"
-			id="btn-linkprev" href="<?php echo get_home_url() ?>/?source=<?php echo get_source_url();?>">
+<header id="header-purchase" class="row">	
+
+	<div id="header-purchase-title" class="col-sm-12 col-xs-12 text-center hidden-sm hidden-md
+ hidden-lg visible-xs">
+			
+		<button 
+			class="btn btn-sm btn-kmibox-white pull-left" 
+			data-target="1" 
+			data-action="prev"
+			id="btn-prev">
 			<i class="fa fa-chevron-left" aria-hidden="true"></i>
 			Atras
-		</a>
-
-		
-
-			<label style="text-align: right;color: #ffffff; font-size: 18px;margin-left: 450px; font-family: caviar_dreamsregular" id="header"></label>
+		</button>
+			<label style="text-align: right;color: #ffffff; font-size: 18px; font-family: caviar_dreamsregular" id="header"></label>
 	</div>
-</header>
+				
+	<div id="header-purchase-title" class="col-sm-12 text-center hidden-xs">
+			
+		<button 
+			class="btn btn-sm btn-kmibox-white pull-left" 
+			data-target="1" 
+			data-action="prev"
+			id="btn-prev">
+			<i class="fa fa-chevron-left" aria-hidden="true"></i>
+			Atras
+		</button>
 
+		<label style="text-align: right;color: #ffffff; font-size: 18px;margin-left: 70%; font-family: caviar_dreamsregular" id="header1"></label>
+	</div>	
+</header>
